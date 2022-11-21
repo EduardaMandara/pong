7889f7300181797bce501520e9a833ee07548138
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2022-11-20 23:22:19 -0300

    subi a pasta .hintrc

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15; 
+
+let velocidadeXBolinha = 7;
+let velocidadeYBolinha = 7;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xBolinha, yBolinha, diametro);
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
