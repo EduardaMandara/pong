c6d1bce254ebcaca9e2971296d6c41a586f8312c
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-02-04 22:58:22 +0000

    >=

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 15; 
-let minhaRaquete = 299
+let diametro = 10; 
+let minhaRaquete = 130;
 
 let velocidadeXBolinha = 7;
 let velocidadeYBolinha = 7;
@@ -17,6 +17,6 @@ function draw() {
   yBolinha += velocidadeYBolinha;
 }
 
-if (xBolinha > 150) {
+if (xBolinha >= 150) {
     velocidadeXBolinha : 6
 } 
