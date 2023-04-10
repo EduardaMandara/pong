8adbeb1d74c0a0043cd062fd256d4fe93315424e
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-04-10 00:15:34 -0300

    up

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,10 @@
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 10; 
+let diametro = 15; 
 let minhaRaquete = 130;
 
-let velocidadeXBolinha = 7;
-let velocidadeYBolinha = 7;
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
 
 function setup() {
   createCanvas(600, 400);
@@ -19,11 +19,11 @@ function draw() {
   //Se o xBolinha for maior que a borda ou menor que 0, alterar a velocidade para o lado contrário.
   if (xBolinha > width || xBolinha < 0) { 
     velocidadeXBolinha *= -1
-  }; 
+  }
 
-  if (yBolinha > heighy || yBolinha< 0) {
+  if (yBolinha > height || yBolinha< 0) {
     velocidadeYBolinha *= -1
-  };
+  }
 }
 
  
