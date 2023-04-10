8affe3cb237d0ef8f62543a1290eeb3fc6540b35
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-04-10 01:31:56 -0300

    update notes

---
+++ assets/notes/note.js
@@ -0,0 +1,16 @@
+'function' //a condição de uma função não fica solta, ela fica dentro dos (parenteses). Assim como usamos {chaves} quando queremos adicionar nossa ação. 
+//Ex: if (condição/regra) então {algo acontece dependendo da minha condição ser verdadeira ou falsa}
+
+'var width = largura e height = altura. '
+
+//Usamos as condicionais para realizar uma verificação de algum aspecto do código e executar uma ação.
+
+//Um conjunto de comandos ou códigos que realiza uma tarefa específica. No nosso caso, cada função criada é responsável por um comportamento, tornando a leitura do código mais intuitiva e simples.
+
+//Diametro é 2x igual ao raio.
+
+'_____________________________________________________________________________________'
+date ()
+title ()
+
+'Movimento minha raquete'
