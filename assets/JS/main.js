e4abdd49f3d9493ffd6eb317c464e69c32d0242a
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-06-06 08:31:20 -0300

    Verificada a colisão da bolinha.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -14,22 +14,24 @@ let xRaquete = 5;
 let yRaquete = 150; 
 let comprimentoRaquete = 10;
 let alturaRaquete = 90; 
+let colidiu = false;
 
 
 function setup() {
   createCanvas(600, 400);
 }
 
+//draw() que vai acionando outras funções;
 function draw() {
   background(0);
     
-  mostraBolinha(); //chama a função que criamos lá embaixo para MOSTRAR A BOLINHA, ajudando a diminuir as coisas que estão dentro da nossa função <draw>;
-  
+  mostraBolinha(); //chama a função que criamos lá embaixo para MOSTRAR A BOLINHA, ajudando a diminuir as coisas que estão dentro da nossa função <draw>
   velocidadeBolinha(); //chama a função que criamos lá embaixo para VELOCIDADE DA BOLINHA;
-  
   colisaoBolinha(); //chama a função que criamos lá embaixo para verificação da COLISÃO DA BOLINHA COM AS BORDAS;
-  
   mostraRaquete (); //chama a função que criamos para MOSTRAR A RAQUETE;
+  movimentaMinhaRaquete(); //movimenta nossa raquete para cima e para baixo;
+  //verificaColisaoRaquete(); //colisão da bolinha com a raquete e com as bordas;
+  colisaoMinhaRaqueteBiblioteca(); //verificação de colisão utilizando a biblioteca de outra pessoa;
 }
 
 function mostraBolinha (){
@@ -53,4 +55,29 @@ function colisaoBolinha () {
   if (yBolinha + raio > height || yBolinha - raio < 0) {
     velocidadeYBolinha *= -1
   }
+}
+
+function movimentaMinhaRaquete() {
+    if (keyIsDown(UP_ARROW)) {
+        yRaquete -= 10;
+    }
+  
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += 10;
+    }
+}
+
+function verificaColisaoRaquete() {
+    if (xBolinha - raio < xRaquete + raqueteComprimento
+        && yBolinha - raio < yRaquete + raqueteAltura
+        && yBolinha + raio > yRaquete) {
+        velocidadeXBolinha *= -1;
+    }
+}
+
+function colisaoMinhaRaqueteBiblioteca() {
+    colidiu = collideRectCircle(xRaquete, yRaquete, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+    if (colidiu) {
+        velocidadeXBolinha *= -1;
+    }
 }
