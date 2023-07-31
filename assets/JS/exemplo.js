2d2543677239aa76ba8b113f765ce0ac4e56373a
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2023-07-31 07:39:42 -0300

    chance de erro do oponente;

--- assets/JS/exemplo.js
+++ assets/JS/exemplo.js
@@ -30,6 +30,8 @@ let ponto;
 let raquetada;
 let trilha;
 
+let chanceDeErrar = 0;
+
 function preload(){
   trilha = loadSound("trilha.mp3");
   raquetada = loadSound("raquetada.mp3");
@@ -134,4 +136,24 @@ function marcaPonto(){
     pontosDoOponente += 1;
     ponto.play();
   }
+}
+
+function movimentaRaqueteOponente(){
+    velocidadeYOponente = yBolinha -yRaqueteOponente - raqueteComprimento / 2 - 30;
+    yRaqueteOponente += velocidadeYOponente + chanceDeErrar
+    calculaChanceDeErrar()
+}
+
+function calculaChanceDeErrar() {
+    if (pontosDoOponente >= meusPontos) {
+      chanceDeErrar += 1
+      if (chanceDeErrar >= 39){
+      chanceDeErrar = 40
+      }
+    } else {
+      chanceDeErrar -= 1
+      if (chanceDeErrar <= 35){
+      chanceDeErrar = 35
+      }
+    }
 }
